32164bcfab1486204fcbdaa7a0816c0d0e59eb2b
s.turbin <s.turbin@bftcom.com>
2022-01-27 13:14:36 +0400

    добавил rest контроллер

--- src/main/java/com/bfrcom/controller/RestController.java
+++ src/main/java/com/bfrcom/controller/RestController.java
@@ -1,12 +1,19 @@
 package com.bfrcom.controller;
 
-/*import org.springframework.web.bind.annotation.GetMapping;
+import com.bfrcom.DAO.BookDAO;
+import com.bfrcom.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.web.bind.annotation.RestController
+@org.springframework.web.bind.annotation.RestController("api")
 public class RestController {
 
-    @GetMapping("hi")
-    public String hi(){
-        return "hi!";
+    @Autowired
+    BookDAO bookDAO;
+
+    @GetMapping("book")
+    public Book[] hi(){
+
+        return  bookDAO.findAll().toArray(new Book[0]);
     }
-}*/
+}
